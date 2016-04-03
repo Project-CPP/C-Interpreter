536d24a0599fd6cbe246e5fb3f4ce9b6de022dee
Jacob Muncy <jacobmuncy0@gmail.com>
2016-04-02 23:43:40 -0400

    Add and update documentation.

--- Console.h
+++ Console.h
@@ -1,36 +1,48 @@
+/*     
+
+	Author: Jaocb Muncy / Project-CPP.
+	Date: 4/2/2016.
+	Purpose: This header is used to handle user input when needing to use the console. This will handle console out and console
+	in when needed by the user. This will also check if you included your library that is needed for your functions to work.
+	
+*/
+	
 #include <Include.h>
 
 using namespace std;
 
+
+// Check to see if the user is trying to console out.
 int ConsoleOutCheck(string ProcessInput)
 {
 
-	// Check for iostream library.
+	// Check to see if the preprocessor directive is included with the name iostream library. If not, tell the user to include it.
 		if (Library.iostream == false)
 		{
 			cout << "\nPlease include the iostream library.\n\n";
 			return 0;
 		}
 
-  // CHECK FOR CONSOLE OUT.
+  // Check for cout function when user inputs infomation into the terminal.
 	if (ProcessInput.substr(0,4) == "cout")
 	  {
-			int FirstQuotation = 0;
-			int LastQuotation = 0;
-			int QuotationCount = 0;
+		int FirstQuotation = 0;
+		int LastQuotation = 0;
+		int QuotationCount = 0;
+			
 		FirstQuotation = ProcessInput.find("\"");
-		if (FirstQuotation != string::npos)
-		{
-			 QuotationCount = 1;
-		}
+			if (FirstQuotation != string::npos)
+				{
+					QuotationCount = 1;
+				}
 		FirstQuotation++;
 		LastQuotation = ProcessInput.substr(FirstQuotation,9920).find("\"");
-		if (LastQuotation != string::npos)
-		{
-			 QuotationCount = 2;
-		}
+			if (LastQuotation != string::npos)
+				{
+					 QuotationCount = 2;
+				}
 
-    // OUTPUT FEEDBACK.
+    // Output feedback.
 		cout << endl << ProcessInput.substr(FirstQuotation,LastQuotation) << endl << endl;
   }
 }
